2b72ee9a9d6687c3d54718cb974fb44ae0b349ed
Sonali <sonalimetkar1@gmail.com>
2022-05-19 13:46:57 +0100

    Final comitt by first user.

---
+++ src/test/java/MavenTraining/Mavenjava/demo.java.java
@@ -0,0 +1,15 @@
+package MavenTraining.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class demo {
+	
+	@Test
+	public void demo1()
+	{
+		System.out.println("testing purpose");
+
+	}
+	
+	
+}
